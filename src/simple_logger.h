258d0e21cd1e5937d67d0b370ec302f5e310cbb8
Carl Kelso <EonDigital@gmail.com>
2024-10-05 00:17:10 -0700

    Make logger automatically add newlines

--- src/simple_logger.h
+++ src/simple_logger.h
@@ -11,9 +11,11 @@
 #define SIMPLE_LOGGER_DEST stdout
 #define SIMPLE_LOGGER_INFO_MSG "INFO> "
 #define SIMPLE_LOGGER_ERROR_MSG "ERROR> "
+#define SLPRE ""
+#define SLSUF "\n"
 
-#define LOG_INFO( fmt, ... ) fprintf( SIMPLE_LOGGER_DEST, SIMPLE_LOGGER_INFO_MSG fmt, ##__VA_ARGS__ )
-#define LOG_ERROR( fmt, ... ) fprintf( SIMPLE_LOGGER_DEST, SIMPLE_LOGGER_ERROR_MSG fmt, ##__VA_ARGS__ )
+#define LOG_INFO( fmt, ... ) fprintf( SIMPLE_LOGGER_DEST, SLPRE SIMPLE_LOGGER_INFO_MSG fmt SLSUF, ##__VA_ARGS__ )
+#define LOG_ERROR( fmt, ... ) fprintf( SIMPLE_LOGGER_DEST, SLPRE SIMPLE_LOGGER_ERROR_MSG fmt SLSUF, ##__VA_ARGS__ )
 
 
 #endif /* SRC_SIMPLE_LOGGER_H_ */
