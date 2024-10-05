2bbe1090e536aeb6f297b6dc59b1722f14d2bd09
Carl Kelso <EonDigital@gmail.com>
2024-10-04 21:46:05 -0700

    Implement a simple logger so we can add more later

---
+++ src/simple_logger.h
@@ -0,0 +1,19 @@
+/**
+ * Copyright Carl Kelso, Oct 4, 2024.  All Rights Reserved.
+ */
+
+#ifndef SRC_SIMPLE_LOGGER_H_
+#define SRC_SIMPLE_LOGGER_H_
+
+
+#include <stdio.h>
+
+#define SIMPLE_LOGGER_DEST stdout
+#define SIMPLE_LOGGER_INFO_MSG "INFO> "
+#define SIMPLE_LOGGER_ERROR_MSG "ERROR> "
+
+#define LOG_INFO( fmt, ... ) fprintf( SIMPLE_LOGGER_DEST, SIMPLE_LOGGER_INFO_MSG fmt, ##__VA_ARGS__ )
+#define LOG_ERROR( fmt, ... ) fprintf( SIMPLE_LOGGER_DEST, SIMPLE_LOGGER_ERROR_MSG fmt, ##__VA_ARGS__ )
+
+
+#endif /* SRC_SIMPLE_LOGGER_H_ */
